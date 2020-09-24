f236694afe4249556e7ceb98cbf2f65e68d0ec10
Slimwoods11 <chriswoods2018@hotmail.com>
2020-09-24 14:02:46 -0500

    sportsgame

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
 let resetbutton = document.querySelector('#reset-button');
 let teamoneshootbutton = document.querySelector('#teamone-shoot-button');
 let teamtwoshootbutton = document.querySelector('#teamtwo-shoot-button');
+let goalsteamone = document.querySelector('#teamone-numgoals');
+let goalsteamtwo = document.querySelector('#teamtwo-numgoals');
 
 const numresets = document.querySelector('#num-resets');
 const teamonenumshots = document.querySelector('#teamone-numshots');
@@ -9,19 +11,30 @@ const teamtwonumshots = document.querySelector('#teamtwo-numshots');
 let shotstakenteamone = 0;
 let shotstakenteamtwo = 0;
 let resetshotstaken = 0;
+let teamonenumgoals = 0;
+let teamtwonumgoals = 0;
 
 teamoneshootbutton.addEventListener('click', function () {
   shotstakenteamone = shotstakenteamone + 1;
   teamonenumshots.innerHTML = shotstakenteamone;
-  let teamonenumgoals = Math.floor(Math.random() * 8 + 1);
-  document.getElementById('teamone-numgoals').innerHTML = teamonenumgoals;
+  if (Math.random() * 100 < 45) {
+    console.log('hi');
+
+    teamonenumgoals = teamonenumgoals + 1;
+    goalsteamone.innerHTML = teamonenumgoals;
+  }
 });
 
 teamtwoshootbutton.addEventListener('click', function () {
   shotstakenteamtwo = shotstakenteamtwo + 1;
   teamtwonumshots.innerHTML = shotstakenteamtwo;
-  let teamtwonumgoals = Math.floor(Math.random() * 8 + 1);
-  document.getElementById('teamtwo-numgoals').innerHTML = teamtwonumgoals;
+
+  if (Math.random() * 100 < 45) {
+    console.log('hi');
+
+    teamtwonumgoals = teamtwonumgoals + 1;
+    goalsteamtwo.innerHTML = teamtwonumgoals;
+  }
 });
 
 resetbutton.addEventListener('click', function () {
@@ -30,7 +43,11 @@ resetbutton.addEventListener('click', function () {
 
   shotstakenteamone = 0;
   shotstakenteamtwo = 0;
+  teamonenumgoals = 0;
+  teamtwonumgoals = 0;
 
   teamonenumshots.innerHTML = shotstakenteamone;
   teamtwonumshots.innerHTML = shotstakenteamtwo;
+  goalsteamone.innerHTML = teamonenumgoals;
+  goalsteamtwo.innerHTML = teamtwonumgoals;
 });
